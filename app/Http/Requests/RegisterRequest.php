2529e65e3bc7639081f32ff3994525412e13e554
KassemHachi <kassem.hachi@outlook.com>
2024-07-02 14:04:08 +0100

    chore(auth) :ceate RegisterRequest

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => ['required','string','max:255'],
+            "email" => ['required','string','email','max:255','unique:users'],
+            "password" => ['required','string','confirmed','min:8'],
+            "type"=> ['required','string','in:admin,doctor,patient'],
+            "phone" => ['required','string','min:10']
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+
+}
