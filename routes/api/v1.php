841958b347f8b80d4396cac03e144ecd6a98e3d4
KassemHachi <kassem.hachi@outlook.com>
2024-07-02 13:27:17 +0100

    chore(auth): create auth endpoitns

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('verifying', [AuthController::class, 'verifying']);
+    Route::post('forget-password', [AuthController::class, 'forgetPassword']);
+    Route::post('reset-password', [AuthController::class, 'resetPassword']);
+});
