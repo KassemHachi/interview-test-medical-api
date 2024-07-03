68ad07d5201d6475c653a56e3a5ea90853b8ec01
KassemHachi <kassem.hachi@outlook.com>
2024-07-03 18:31:31 +0100

    chore(appointment): add delete function

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Enums\UserTypeEnum;
 use App\Http\Requests\CreateAppointmentRequest;
+use App\Http\Requests\DeleteAppointmentRequest;
 use App\Http\Requests\GetAllAppointments;
 use App\Http\Requests\GetOneAppointment;
 use App\Http\Requests\UpdateAppointmentRequest;
 use App\Http\Resources\AppointmentResource;
 use App\Models\User;
 use App\Services\AppointmentService;
-use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
@@ -41,13 +41,28 @@ class AppointmentController extends Controller
 
     public function get(int $id,GetOneAppointment $request)
     {
-        $appointment = $this->appointmentService->get($id);
+        $appointment = $this->appointmentService->get($id, $request->user());
+        if (!$appointment) {
+            return $this->error("Appointment not found", 404);
+        }
         return new AppointmentResource($appointment);
     }
 
     public function update(int $id, UpdateAppointmentRequest $request)
     {
-        $appointment = $this->appointmentService->update($id,$request->validated());
+        $appointment = $this->appointmentService->update($id,$request->validated(), $request->user());
+        if (!$appointment) {
+            return $this->error("Appointment not found", 404);
+        }
         return new AppointmentResource($appointment);
+    }
+
+    public function delete(int $id,DeleteAppointmentRequest $request)
+    {
+        $isDeleted = $this->appointmentService->delete($id, $request->user());
+        if (!$isDeleted) {
+            return $this->error("Appointment not found", 404);
+        }
+        return $this->success("Appointment deleted successfully");
     }
 }
