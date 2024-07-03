b20db406eeacf60cd77e8bf9855db261b13c8377
KassemHachi <kassem.hachi@outlook.com>
2024-07-03 11:04:05 +0100

    chore: edit send verification request

--- app/Http/Requests/SendVerificationEmailRequest.php
+++ app/Http/Requests/SendVerificationEmailRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class SendVerificationEmailRequest extends FormRequest
 {
@@ -24,5 +26,12 @@ class SendVerificationEmailRequest extends FormRequest
         return [
             //
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+        ], 422));
     }
 }
