7ac6c943500689f39893b857453ebb3b7aa1d13f
KassemHachi <kassem.hachi@outlook.com>
2024-07-03 12:47:15 +0100

    feat(appointment): create appointment model

---
+++ app/Models/Appointment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'date',
+        'time',
+        'patient_id',
+        'doctor_id',
+        'reason',
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(User::class, 'patient_id');
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
+}
