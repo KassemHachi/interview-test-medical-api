913f0b73a5893363e06c04167be014031f38a903
KassemHachi <kassem.hachi@outlook.com>
2024-07-03 11:05:58 +0100

    chore(doctor): add get function in DoctorController

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\UserTypeEnum;
+use App\Http\Requests\GetOneDoctorRequest;
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use App\Services\AccountService;
 use Illuminate\Http\Request;
 
@@ -13,5 +16,13 @@ class DoctorController extends Controller
     public function index(){
         $doctors = $this->accountService->getDoctors();
         return UserResource::collection($doctors);
+    }
+
+    public function get($id){
+        $doctor = User::find($id);
+        if(!$doctor || $doctor->type != UserTypeEnum::DOCTOR->value) {
+            return $this->error("Doctor not found", 404);
+        }
+        return new UserResource($doctor);
     }
 }
