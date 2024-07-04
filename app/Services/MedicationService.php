c4995fc35abee9de375e3a19f50a396d5037e71f
KassemHachi <kassem.hachi@outlook.com>
2024-07-04 19:40:03 +0100

    chore(medication): create all crud functions in service

--- app/Services/MedicationService.php
+++ app/Services/MedicationService.php
@@ -2,7 +2,77 @@
 
 namespace App\Services;
 
+use App\Models\Medication;
+use Illuminate\Database\Eloquent\Collection;
+
 class MedicationService
 {
+
+    /**
+     * Create a new medication
+     *
+     * @param array $data
+     * @return Medication
+     */
+    public function createMedication(array $data):Medication
+    {
+        return Medication::create($data);
+    }
+
+    /**
+     * Update a medication
+     *
+     * @param integer $id
+     * @param array $data
+     * @return Medication
+     */
+    public function updateMedication(int $id, array $data):Medication|null
+    {
+        $medication = $this->getMedication($id);
+        if ($medication){
+            $medication->update($data);
+        }
+        return $medication;
+    }
+
+    /**
+     * Get a medication
+     *
+     * @param integer $id
+     * @return Medication|null
+     */
+    public function getMedication(int $id):Medication|null
+    {
+        $medication =  Medication::findorFail($id);
+        if (!$medication){
+            return null;
+        }
+        return  $medication;
+    }
+
+    /**
+     * Get all medications
+     *
+     * @return Collection
+     */
+    public function getAllMedications():Collection
+    {
+        return Medication::all();
+    }
+
+    /**
+     * Delete a medication
+     *
+     * @param integer $id
+     * @return boolean
+     */
+    public function deleteMedication(int $id):bool
+    {
+        $medication = $this->getMedication($id);
+        if (!$medication){
+            return false;
+        }
+       return $medication->delete();
+    }
 
 }
