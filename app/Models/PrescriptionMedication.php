ab83af31307f5e67f1a1e5dad3c8f51ddb863c84
KassemHachi <kassem.hachi@outlook.com>
2024-07-04 17:25:13 +0100

    db(prescription): create models (Prescription,Medication,PrescriptionMedication)

---
+++ app/Models/PrescriptionMedication.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PrescriptionMedication extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function prescription()
+    {
+        return $this->belongsTo(Prescription::class);
+    }
+
+    public function medication()
+    {
+        return $this->belongsTo(Medication::class);
+    }
+}
