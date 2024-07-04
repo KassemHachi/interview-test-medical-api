71c35b1be2dec4014a490a51e3e18940628047ed
KassemHachi <kassem.hachi@outlook.com>
2024-07-04 19:46:34 +0100

    chore(medication): create index function in controller

--- app/Http/Controllers/MedicationController.php
+++ app/Http/Controllers/MedicationController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GetAllMedicationsRequest;
+use App\Http\Resources\MedicationResource;
 use App\Services\MedicationService;
 
 class MedicationController extends Controller
 {
     public function __construct(protected MedicationService $medicationService){}
 
-    public function index()
+    public function index(GetAllMedicationsRequest $request)
     {
-        //
+        $medications = $this->medicationService->getAllMedications();
+        return new MedicationResource($medications);
     }
 
     public function store()
