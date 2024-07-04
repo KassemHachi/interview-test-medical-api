2e699f72596cf63dd096e0c151622ee6fed06c56
KassemHachi <kassem.hachi@outlook.com>
2024-07-04 17:27:46 +0100

    chore(prescription): create requests
    DeletePrescriptionRequest
    CreatePrescriptionRequest
    GetPrescriptionRequest
    GetAllPrescriptionsRequest

---
+++ app/Http/Requests/UpdatePrescriptionRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\UserTypeEnum;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePrescriptionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->type == UserTypeEnum::DOCTOR->value;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'patient_id' => ['required','exists:users,id'],
+            'prescription_medications' => ['required','array','min:1'],
+            'prescription_medications.*.medication_id' => ['required','exists:medications,id'],
+            'prescription_medications.*.dosage' => ['required','string'],
+            'prescription_medications.*.frequency' => ['required','string'],
+            'prescription_medications.*.start_date' => ['required','date'],
+            'prescription_medications.*.end_date' => ['required','date', 'after_or_equal:prescription_medications.*.start_date'],
+            'notes' =>['string']
+         ];
+    }
+}
