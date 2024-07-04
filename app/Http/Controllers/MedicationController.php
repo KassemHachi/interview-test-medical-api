7148be2b45c0d07ee37873ac2e96bab31ee4ae52
KassemHachi <kassem.hachi@outlook.com>
2024-07-04 19:24:03 +0100

    chore(medication): create medication controller

---
+++ app/Http/Controllers/MedicationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\MedicationService;
+
+class MedicationController extends Controller
+{
+    public function __construct(protected MedicationService $medicationService){}
+
+    public function index()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show()
+    {
+        //
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+}
