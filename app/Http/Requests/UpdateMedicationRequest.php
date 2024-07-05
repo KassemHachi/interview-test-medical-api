730d15ee5ac28ee5eb44ce293a9e2a7984cd0064
KassemHachi <kassem.hachi@outlook.com>
2024-07-05 12:30:16 +0100

    chore(medication): edit UpdateMedicationRequest

--- app/Http/Requests/UpdateMedicationRequest.php
+++ app/Http/Requests/UpdateMedicationRequest.php
@@ -22,7 +22,8 @@ class UpdateMedicationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name'=> ['required','string','max:255'],
+            'description'=> ['required','string','max:255'],
         ];
     }
 }
