91c6dc90cd44036ead5455742887b504dfe585c1
KassemHachi <kassem.hachi@outlook.com>
2024-07-05 14:33:46 +0100

    chore(medical history): create controller

---
+++ app/Http/Controllers/MedicalHistoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MedicalHistoryController extends Controller
+{
+    public function index()
+    {
+       //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show()
+    {
+        //
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+}
