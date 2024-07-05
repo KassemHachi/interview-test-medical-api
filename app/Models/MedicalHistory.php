c5f9602caf27144b7d110e40515b3a7116556709
KassemHachi <kassem.hachi@outlook.com>
2024-07-05 14:20:09 +0100

    feat(medical history): create medical history model

---
+++ app/Models/MedicalHistory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MedicalHistory extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function patient()
+    {
+        return $this->belongsTo(User::class, 'patient_id');
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
+}
